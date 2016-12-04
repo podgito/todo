70dd7c84574c15f9de199f2eb8b21013ec580f9a
podgito <padraic.duffy.1@gmail.com>
2016-12-04 22:10:50 +0000

    dnd demo page

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -1,5 +1,5 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('app')
@@ -10,12 +10,28 @@
         var vm = this;
 
         $scope.tasks = [
-            {name:'Task 1', hours:2},
-            {name:'Task 2', hours:3},
-            {name:'Task 3', hours:1},
-            {name:'Task 3', hours:2}
+            { name: 'Task 1', hours: 2 },
+            { name: 'Task 2', hours: 3 },
+            { name: 'Task 3', hours: 1 },
+            { name: 'Task 4', hours: 2 }
         ]
         
+        $scope.models = {
+            selected: null,
+            lists: { "A": [], "B": [] }
+        };
+
+        // Generate initial model
+        for (var i = 1; i <= 3; ++i) {
+            $scope.models.lists.A.push({ label: "Item A" + i });
+            $scope.models.lists.B.push({ label: "Item B" + i });
+        }
+
+        // Model to JSON for demo purpose
+        $scope.$watch('models', function (model) {
+            $scope.modelAsJson = angular.toJson(model, true);
+        }, true);
+
 
         activate();
 
