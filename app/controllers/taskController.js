e56ff26ec7b26e570c1c8f33465154bd739812f7
Padraic Duffy <padraic.duffy.1@gmail.com>
2016-12-04 20:33:45 +0000

    angular dnd directive

--- app/controllers/taskController.js
+++ app/controllers/taskController.js
@@ -8,6 +8,13 @@
     taskController.$inject = ['$scope'];
     function taskController($scope) {
         var vm = this;
+
+        $scope.tasks = [
+            {name:'Task 1', hours:2},
+            {name:'Task 2', hours:3},
+            {name:'Task 3', hours:1},
+            {name:'Task 3', hours:2}
+        ]
         
 
         activate();
